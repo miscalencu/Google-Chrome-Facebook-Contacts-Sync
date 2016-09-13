d3895031ea88ec5422d5ccac50491abacedd1b8a
George Miscalencu <george.miscalencu@gmail.com>
2016-09-13 17:48:16 +0300

    Initial commit

---
+++ js/core.js
@@ -0,0 +1,11 @@
+﻿function GetContacts() {
+    $.ajax({
+        url: "https://www.facebook.com/george.miscalencu/friends?source_ref=pb_friends_tl",
+        success: function (result) {
+            document.write(result);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert("Error: " + thrownError);
+        }
+    });
+}
